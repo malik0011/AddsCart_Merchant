75a6db746c158a929f1fea4c187637a9c13d90bb
spandanpande <spandansp.nsk@gmail.com>
2022-03-08 12:23:50 +0530

    UI Done

---
+++ app/src/main/java/com/example/addscartmerchant/HomePage.java
@@ -0,0 +1,63 @@
+package com.example.addscartmerchant;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HomePage extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageButton img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+        drawerLayout =findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        img = findViewById(R.id.Img);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigationView.bringToFront();
+                ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(HomePage.this,drawerLayout,R.string.dummy_content,R.string.dummy_content);
+                drawerLayout.addDrawerListener(toggle);
+                toggle.syncState();
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.history:
+                        Intent intent = new Intent(HomePage.this,OrderHistory.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.adminPanel:
+                        Intent intent1 = new Intent(HomePage.this,AdminPanel.class);
+                        startActivity(intent1);
+                        break;
+
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+}
